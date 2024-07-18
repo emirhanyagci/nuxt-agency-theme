0ce7addf22be9b518386ec016f1788db231ce959
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-18 18:37:52 +0300

    feat:back to top functionality added

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,17 @@ export default function executeJs() {
           $(this).remove();
         });
     }
-
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $(".back-to-top").fadeIn("slow");
+      } else {
+        $(".back-to-top").fadeOut("slow");
+      }
+    });
+    $(".back-to-top").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+      return false;
+    });
     /*--/ Carousel owl /--*/
     $("#carousel").owlCarousel({
       loop: true,
