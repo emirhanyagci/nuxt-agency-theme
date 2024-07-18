16d1679aa7668c151df44cb812095373b6ff2904
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-18 15:57:20 +0300

    feat:Add components for pages

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+<template>
+  <!--/ Carousel Star /-->
+  <Carousel />
+  <!--/ Carousel end /-->
+
+  <!--/ Services Star /-->
+  <Services />
+  <!--/ Services End /-->
+  <!--/ Property Star /-->
+  <Property />
+  <!--/ Property End /-->
+
+  <!--/ Agents Star /-->
+  <Agents />
+  <!--/ Agents End /-->
+
+  <!--/ News Star /-->
+  <News />
+  <!--/ News End /-->
+
+  <!--/ Testimonials Star /-->
+  <Testimonials />
+  <!--/ Testimonials End /-->
+</template>
