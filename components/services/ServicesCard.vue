cbf82de1f95f459232139168b92117a7ca185a96
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-18 19:32:16 +0300

    feat:services section completed

---
+++ components/services/ServicesCard.vue
@@ -0,0 +1,34 @@
+<script setup>
+const { icon, header, text, to } = defineProps([
+  "icon",
+  "header",
+  "text",
+  "to",
+]);
+</script>
+
+<template>
+  <div class="col-md-4">
+    <div class="card-box-c foo">
+      <div class="card-header-c d-flex">
+        <div class="card-box-ico">
+          <span :class="`fa ${icon}`"></span>
+        </div>
+        <div class="card-title-c align-self-center">
+          <h2 class="title-c">{{ header }}</h2>
+        </div>
+      </div>
+      <div class="card-body-c">
+        <p class="content-c">
+          {{ text }}
+        </p>
+      </div>
+      <div class="card-footer-c">
+        <a :href="to" class="link-c link-icon"
+          >Read more
+          <span class="ion-ios-arrow-forward"></span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ components/services/ServicesSection.vue
@@ -0,0 +1,47 @@
+<script setup>
+const serviceItems = [
+  {
+    header: "Lifestyle",
+    icon: "fa-gamepad",
+    text: "Sed porttitor lectus nibh. Cras ultricies ligula sed magna dictum porta. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.",
+    to: "#",
+  },
+  {
+    header: "Loans",
+    icon: "fa-usd",
+    text: "Nulla porttitor accumsan tincidunt. Curabitur aliquet quam id dui posuere blandit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.",
+    to: "#",
+  },
+  {
+    header: "Sell",
+    icon: "fa-home",
+    text: "Sed porttitor lectus nibh. Cras ultricies ligula sed magna dictum porta. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.",
+    to: "#",
+  },
+];
+</script>
+<template>
+  <section class="section-services section-t8">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="title-wrap d-flex justify-content-between">
+            <div class="title-box">
+              <h2 class="title-a">Our Services</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <ServicesCard
+          v-for="(item, index) in serviceItems"
+          :key="index"
+          :icon="item.icon"
+          :header="item.header"
+          :text="item.text"
+          :to="item.to"
+        />
+      </div>
+    </div>
+  </section>
+</template>
