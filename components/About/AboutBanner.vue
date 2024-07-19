dcd456d6e56708d6b17ebeb75479ef3c182ee010
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-19 12:22:15 +0300

    feat:page header component created and about page completed

---
+++ components/About/AboutBanner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col-sm-12">
+    <div class="about-img-box">
+      <img src="/img/slide-about-1.jpg" alt="" class="img-fluid" />
+    </div>
+    <div class="sinse-box">
+      <h3 class="sinse-title">
+        EstateAgency
+        <span></span>
+        <br />
+        Sinse 2017
+      </h3>
+      <p>Art & Creative</p>
+    </div>
+  </div>
+</template>
---
+++ components/PageHeader.vue
@@ -0,0 +1,40 @@
+<script setup>
+const router = useRouter();
+const currentRoute = ref(router.currentRoute.value.name);
+
+currentRoute.value =
+  currentRoute.value.charAt(0).toUpperCase() + currentRoute.value.slice(1);
+</script>
+<template>
+  <section class="intro-single">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-8">
+          <div class="title-single-box">
+            <h1 class="title-single">
+              <slot name="title" />
+            </h1>
+            <span v-if="$slots.content" class="color-text-a"
+              ><slot name="content"
+            /></span>
+          </div>
+        </div>
+        <div class="col-md-12 col-lg-4">
+          <nav
+            aria-label="breadcrumb"
+            class="breadcrumb-box d-flex justify-content-lg-end"
+          >
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <a href="#">Home</a>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">
+                {{ currentRoute }}
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
