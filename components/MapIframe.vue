2ab731234697b5315956eff75192d1b681e05e71
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-21 01:22:38 +0300

    feat:contact page completed

---
+++ components/MapIframe.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { src } = defineProps(["src"]);
+</script>
+<template>
+  <div class="contact-map box">
+    <div id="map" class="contact-map">
+      <iframe
+        :src
+        width="100%"
+        height="450"
+        frameborder="0"
+        style="border: 0"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
